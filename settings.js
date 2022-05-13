f5b33426cded9c7af1a1a32a0e3e45463548a843
Modern72 <105321303+Modern72@users.noreply.github.com>
2022-05-13 14:17:19 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@
 //or copy the codes/script,
 //pls give credit
 //no credit? i will take action immediately
-//© 2022 Xeon Bot Inc. Doge Bot 
-//Thank you to Lord Buddha, Family and Myself
+//© 2022 PTRN Bot Inc. PTRN
+//Thank you
 //════════════════════════════//
 
 const fs = require('fs')
@@ -21,18 +21,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['916909137213']
-global.premium = ['916909137213']
-global.ownernomer = '916909137213'
-global.ownername = '🦄Dream Guy Xeon'
-global.botname = 'Cheems Bot MD V2'
-global.footer = 'Xeon Bot Inc.'
-global.ig = 'https://github.com/DGXeon'
-global.region = 'India, NorthEast, Mizoram'
-global.sc = 'https://github.com/DGXeon/CheemsBot-MD2'
-global.myweb = 'https://youtu.be/imFIX-Wrt3s'
-global.packname = 'Cheems Bot'
-global.author = '🦄Dream Guy Xeon'
+global.owner = ['2222']
+global.premium = ['2222']
+global.ownernomer = '2222'
+global.ownername = 'PTRN '
+global.botname = 'PTRN BOT'
+global.footer = 'PTRN Bot Inc.'
+global.ig = 'https://github.com/'
+global.region = 'morocco, middleEast, tetouan'
+global.sc = 'https://github.com/'
+global.myweb = 'ntg'
+global.packname = 'PTRN Bot'
+global.author = 'PTRN'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
